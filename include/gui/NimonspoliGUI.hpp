f270dde772a1d41c845ddcb720562d61770132e6
HussainDzaki <163353530+HussainDzaki@users.noreply.github.com>
2026-04-26 21:23:57 +0700

    Chore : cleaning Unused File

--- include/gui/NimonspoliGUI.hpp
+++ include/gui/NimonspoliGUI.hpp
@@ -5,10 +5,7 @@
 
 #include "Boardview/BoardRenderer.hpp"
 #include "Boardview/BuildingRenderer.hpp"
-#include "Boardview/CenterRenderer.hpp"
 #include "Boardview/TileRenderer.hpp"
-
-#include "DynamicEntity/DiceRenderer.hpp"
 #include "DynamicEntity/PawnRenderer.hpp"
 
 #include "MathCore/IsoTransformer.hpp"
